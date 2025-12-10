26d1cc383449f41fccf94e20795553c546a43ce1
Jung Bum Lee <99597711+felix9698@users.noreply.github.com>
2025-12-10 16:04:06 +0900

    Add new alumni data for Heesoo and Hong Ki Kim
    Added new alumni entries with images and links.

--- data/people/alumnis.ts
+++ data/people/alumnis.ts
@@ -2,6 +2,17 @@ import { Alumni } from "@data/_types/people_types";
 
 export const ALUMNIS: Alumni[] = [
   {
+    image: "/people/students/heesoo-kim.jpg",
+    name: "Heesoo Kim",
+    github: "https://github.com/happywater12",
+  },
+  {
+    image: "/people/students/hongki-kim.jpg",
+    name: "Hong Ki Kim",
+    website:
+      "https://ihatemushroom.notion.site/About-Hong-Ki-Kim-129a76a7e22e80139125c0f2d66d2d09",
+  },
+  {
     image: "/people/alumni/minji-kim.jpg",
     name: "Minji Kim",
     website: "https://github.com/kimminji1013",
