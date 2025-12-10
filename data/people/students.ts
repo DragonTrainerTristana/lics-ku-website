c4e0cbda86bf27a596c6e0925fc93fc590dc32e9
Jung Bum Lee <99597711+felix9698@users.noreply.github.com>
2025-12-10 16:03:28 +0900

    Update student lists by modifying PHD and MASTER students
    Removed several PHD students and added Youngjin Song to MASTER_STUDENTS.

--- data/people/students.ts
+++ data/people/students.ts
@@ -2,17 +2,6 @@ import { Student } from "@data/_types/people_types";
 
 export const PHD_STUDENTS: Student[] = [
   {
-    image: "/people/students/heesoo-kim.jpg",
-    name: "Heesoo Kim",
-    github: "https://github.com/happywater12",
-  },
-  {
-    image: "/people/students/hongki-kim.jpg",
-    name: "Hong Ki Kim",
-    website:
-      "https://ihatemushroom.notion.site/About-Hong-Ki-Kim-129a76a7e22e80139125c0f2d66d2d09",
-  },
-  {
     image: "/people/students/yonghun-jang.jpg",
     name: "Yong Hun Jang",
     email: "disclose@korea.ac.kr",
@@ -49,18 +38,19 @@ export const PHD_STUDENTS: Student[] = [
     website: "https://scholar.google.co.kr/citations?user=h5rKGYAAAAAJ&hl=ko",
   },
   {
-    image: "/people/students/youngjin-song.jpg",
-    name: "Youngjin Song",
-    linkedin: "https://www.linkedin.com/in/youngjinsong4090/",
-  },
-  {
     image: "/people/students/gun-kim.jpg",
     name: "Gun Kim",
     github: "https://github.com/brandonkims",
   },
 ];
 
-export const MASTER_STUDENTS: Student[] = [];
+export const MASTER_STUDENTS: Student[] = [
+    {
+    image: "/people/students/youngjin-song.jpg",
+    name: "Youngjin Song",
+    linkedin: "https://www.linkedin.com/in/youngjinsong4090/",
+  },
+];
 
 export const UNDERGRADUATE_STUDENTS: Student[] = [
   {
