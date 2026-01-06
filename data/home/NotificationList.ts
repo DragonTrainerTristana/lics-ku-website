3cea2cfa8028d8d6e7747920d290a1f046f69a94
Seung Hyun OH <48664307+DragonTrainerTristana@users.noreply.github.com>
2026-01-06 13:30:12 +0900

    수정

--- data/home/NotificationList.ts
+++ data/home/NotificationList.ts
@@ -20,7 +20,12 @@ export const NOTIFICATION_LIST: Notification[] = [
   {
     id: 3,
     title: "대학원생 모집 공고",
-    description: "현재 LICS 연구실에서 대학원생을 모집하고 있습니다.",
+    description:
+      "저희 연구실은 차세대 통신, 네트워크, 그리고 디지털 트윈을 융합한 연구를 수행하고 있습니다.\n" +
+      "물리적 통신 네트워크 환경을 가상 공간에 정밀하게 구현하고, 이를 통해 지능형 네트워크 최적화 및 예측 기술을 개발하는 것을 목표로 하고 있습니다.\n\n" +
+      "대학원 진학과 관련하여 궁금한 것이 있으면 교수님께 연락주세요.\n" +
+      "지도교수 : 이상현 교수님 (공학관 407호 sanghyunlee@korea.ac.kr)\n" +
+      "방장 : 오승현 (신공학관 534호 seunghyunoh@korea.ac.kr)",
     createdAt: "2025-08-08",
     resources: [
       {
